6436e3b175f0a2ae8da9ef6b0391516cf60aa8e7
Xalcon <xalcon@xalcon.net>
2017-06-14 13:21:02 +0200

    Added view changing support via tabs Added missing textures (oops)

--- src/main/java/net/xalcon/technomage/client/gui/technonomicon/GuiTechnonomiconResearchViewer.java
+++ src/main/java/net/xalcon/technomage/client/gui/technonomicon/GuiTechnonomiconResearchViewer.java
@@ -1,25 +1,34 @@
 package net.xalcon.technomage.client.gui.technonomicon;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.RenderHandEvent;
 import net.xalcon.technomage.Technomage;
 import net.xalcon.technomage.api.technonomicon.*;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.Point;
 import org.lwjgl.util.Rectangle;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GuiTechnonomiconResearchViewer extends GuiScreen
 {
     private final static ResourceLocation RESEARCH_TEX = new ResourceLocation(Technomage.MOD_ID, "textures/gui/technonomicon/research.png");
     private final static int INDENT = 8;
     private List<IResearchView> viewList = new ArrayList<>();
+    private IResearchView activeView = null;
 
     private int winScaleFactor;
     private int guiWidth;
@@ -31,6 +40,8 @@ public class GuiTechnonomiconResearchViewer extends GuiScreen
         super.initGui();
         this.guiWidth = 256 + 96;
         this.guiHeight = 224;
+
+        this.viewList.clear();
 
         ResearchView view = new ResearchView(new ResourceLocation(Technomage.MOD_ID, "test"), new ItemStackGuiIcon(Items.GOLD_NUGGET));
         view.addResearchItem(new IResearchViewItem()
@@ -77,6 +88,20 @@ public class GuiTechnonomiconResearchViewer extends GuiScreen
             }
         });
         this.viewList.add(view);
+
+        view = new ResearchView(new ResourceLocation(Technomage.MOD_ID, "test2"), new ItemStackGuiIcon(Items.DIAMOND));
+        view.setBackgroundTexture(new ResourceLocation(Technomage.MOD_ID, "textures/gui/technonomicon/background_02.png"));
+        this.viewList.add(view);
+
+        view = new ResearchView(new ResourceLocation(Technomage.MOD_ID, "test3"), new ItemStackGuiIcon(Items.REDSTONE));
+        view.setBackgroundTexture(new ResourceLocation(Technomage.MOD_ID, "textures/gui/technonomicon/background_03.png"));
+        this.viewList.add(view);
+
+        view = new ResearchView(new ResourceLocation(Technomage.MOD_ID, "test4"), new ItemStackGuiIcon(Blocks.CRAFTING_TABLE));
+        view.setBackgroundTexture(new ResourceLocation(Technomage.MOD_ID, "textures/gui/technonomicon/background_02.png"));
+        this.viewList.add(view);
+
+        this.activeView = this.viewList.get(0);
     }
 
     @Override
@@ -84,6 +109,38 @@ public class GuiTechnonomiconResearchViewer extends GuiScreen
     {
         super.setWorldAndResolution(mc, width, height);
         this.winScaleFactor = new ScaledResolution(mc).getScaleFactor();
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
+    {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        int left = (this.width - this.guiWidth) / 2;
+        int top = (this.height - this.guiHeight) / 2;
+
+        Point mousePos = new Point(mouseX - left, mouseY - top);
+        List<IResearchView> list = this.viewList.stream()
+                .filter(IResearchView::isVisible)
+                .sorted(Comparator.comparingInt(IResearchView::getSortingIndex))
+                .collect(Collectors.toList());
+        int i = 0;
+        for(IResearchView view : list)
+        {
+            Rectangle bounds = new Rectangle(0, 48 + 18 * i, 16, 16);
+            if(bounds.contains(mousePos))
+            {
+                this.mc.getSoundHandler().playSound(PositionedSoundRecord.getMasterRecord(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+                this.activeView = view;
+                return;
+            }
+            i++;
+        }
+    }
+
+    @Override
+    protected void mouseReleased(int mouseX, int mouseY, int state)
+    {
+        super.mouseReleased(mouseX, mouseY, state);
     }
 
     @Override
@@ -98,8 +155,7 @@ public class GuiTechnonomiconResearchViewer extends GuiScreen
         Point relMousePos = new Point(mouseX - left, mouseY - top);
         this.drawDefaultBackground();
 
-        IResearchView view = this.viewList.stream().findFirst().orElse(null);
-        if(view == null) return;
+        if(this.activeView == null) return;
 
         GlStateManager.pushMatrix();
         GlStateManager.enableBlend();
@@ -110,22 +166,47 @@ public class GuiTechnonomiconResearchViewer extends GuiScreen
         GL11.glEnable(GL11.GL_SCISSOR_TEST);
         GL11.glScissor((left + INDENT) * this.winScaleFactor, (top + INDENT) * this.winScaleFactor,
                 (this.guiWidth - INDENT * 2) * this.winScaleFactor, (this.guiHeight - INDENT * 2) * this.winScaleFactor);
-        view.renderBackgroundLayer(viewport, relMousePos);
-        view.renderForegroundLayer(viewport, relMousePos);
+        this.activeView.renderBackgroundLayer(viewport, relMousePos);
+        this.activeView.renderForegroundLayer(viewport, relMousePos);
         GL11.glDisable(GL11.GL_SCISSOR_TEST);
 
-        this.drawTabs();
         this.drawBorder();
+        this.drawTabs(relMousePos);
         if(viewport.contains(mouseX, mouseY))
-            view.renderTooltipLayer(viewport, relMousePos);
+            this.activeView.renderTooltipLayer(viewport, relMousePos);
         else
             this.handleTabMouseOver();
 
         GlStateManager.popMatrix();
     }
 
-    private void drawTabs()
+    private void drawTabs(Point mousePos)
     {
+        List<IResearchView> list = this.viewList.stream()
+                .filter(IResearchView::isVisible)
+                .sorted(Comparator.comparingInt(IResearchView::getSortingIndex))
+                .collect(Collectors.toList());
+        int i = 0;
+        RenderHelper.enableGUIStandardItemLighting();
+        for(IResearchView view : list)
+        {
+            Rectangle bounds = new Rectangle(0, 48 + 18 * i, 16, 16);
+
+            this.mc.getTextureManager().bindTexture(RESEARCH_TEX);
+            if(view != this.activeView)
+                GlStateManager.color(0.5f, 0.5f, 0.5f);
+            this.drawTexturedModalRect(-16, 32 + 18 * i, 0, 0, 48, 48);
+            GlStateManager.color(1f, 1f, 1f);
+            if(bounds.contains(mousePos))
+            {
+                //GlStateManager.blendFunc(GlStateManager.SourceFactor.DST_COLOR, GlStateManager.DestFactor.ONE);
+                drawRect(bounds.getX(), bounds.getY(), bounds.getX() + bounds.getWidth(), bounds.getY() + bounds.getHeight(), 0x88FFFFFF);
+                //GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            }
+
+            view.getIcon().renderAt(0, 48 + 18 * i);
+            i++;
+        }
     }
 
     private void handleTabMouseOver()
