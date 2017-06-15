4f8fd13d44ad6624a69430d70f6fdda645019827
Xalcon <xalcon@xalcon.net>
2017-06-15 13:00:17 +0200

    tried to reimplement zoom, failed miserably. I guess i'll revisit this later

--- src/main/java/net/xalcon/technomage/api/gui/technonomicon/ResearchView.java
+++ src/main/java/net/xalcon/technomage/api/gui/technonomicon/ResearchView.java
@@ -1,11 +1,15 @@
 package net.xalcon.technomage.api.gui.technonomicon;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.xalcon.technolib.client.GuiHelper;
 import net.xalcon.technomage.Technomage;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.Rectangle;
 
 import java.util.ArrayList;
@@ -24,7 +28,7 @@ public class ResearchView implements IResearchView
     private int y;
     private int max_width = 512;
     private int max_height = 512;
-    private float zoom;
+    private float zoom = 1;
     private int lastMouseX;
     private int lastMouseY;
 
@@ -87,8 +91,14 @@ public class ResearchView implements IResearchView
     @Override
     public void onShow(GuiScreen parent)
     {
-        this.x = 0;
-        this.y = 0;
+        this.setToCenter();
+    }
+
+    public void setToCenter()
+    {
+        this.x = -((this.max_width - this.bounds.getWidth()) / 2);
+        this.y = -((this.max_height - this.bounds.getHeight()) / 2);
+        this.zoom = 1;
     }
 
     public void setBackgroundTexture(ResourceLocation location)
@@ -107,8 +117,8 @@ public class ResearchView implements IResearchView
         GuiHelper.INSTANCE.bindTexture(RESEARCH_TEXTURE);
         Rectangle itemBounds = item.getBounds();
 
-        int rx = this.x + this.bounds.getX() + itemBounds.getX();
-        int ry = this.y + this.bounds.getY() + itemBounds.getY();
+        int rx = (int) ((this.x + itemBounds.getX()) * this.zoom + this.bounds.getX());
+        int ry = (int) ((this.y + itemBounds.getY()) * this.zoom + this.bounds.getY());
         GuiHelper.INSTANCE.drawTexturedModalRect( rx - 5, ry - 5, 42 + 26 * item.getResearchIconType().getIndex(), 107, 26, 26, 0);
         GlStateManager.color(1f, 1f, 1f);
         GlStateManager.disableLighting();
@@ -119,14 +129,29 @@ public class ResearchView implements IResearchView
     @Override
     public void renderComponent(int mouseX, int mouseY)
     {
+        int wheel = Mouse.getDWheel();
+        if (wheel > 0)
+            this.zoom += 0.10f;
+        else if (wheel < 0)
+            this.zoom -= 0.10f;
+        this.zoom = MathHelper.clamp(this.zoom, 0.5f, 1.0f);
+
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(this.zoom, this.zoom, this.zoom);
         GuiHelper.INSTANCE.bindTexture(this.background);
-        GuiHelper.INSTANCE.drawTexturedModalRect(this.bounds.getX(), this.bounds.getY(), -this.x, -this.y,
-                this.bounds.getWidth(), this.bounds.getHeight(), 0);
+        GuiHelper.INSTANCE.drawTexturedModalRect(this.bounds.getX() / this.zoom, this.bounds.getY() / this.zoom, -this.x, -this.y,
+                (int)(this.bounds.getWidth() * this.zoom), (int)(this.bounds.getHeight() * this.zoom), 0);
+
+        GL11.glDisable(GL11.GL_SCISSOR_TEST);
+        Minecraft.getMinecraft().fontRendererObj.drawString(this.x + "/" + this.y, 0, 0, -1);
+        GL11.glEnable(GL11.GL_SCISSOR_TEST);
 
         RenderHelper.enableGUIStandardItemLighting();
         for (IResearchViewItem item: this.researchTree)
-            renderItem(item);
+            this.renderItem(item);
         RenderHelper.disableStandardItemLighting();
+        GlStateManager.popMatrix();
+
     }
 
     @Override
@@ -154,8 +179,8 @@ public class ResearchView implements IResearchView
         int deltaX = mouseX - this.lastMouseX;
         int deltaY = mouseY - this.lastMouseY;
 
-        this.x += deltaX;
-        this.y += deltaY;
+        this.x += deltaX / this.zoom;
+        this.y += deltaY / this.zoom;
 
         this.lastMouseX = mouseX;
         this.lastMouseY = mouseY;
